bc8ad8a7cf3eccdce8ec948849cb4ae0e3630b81
shahajahanahamed <ahamedshahajahan@gmail.com>
2023-02-10 20:51:31 +0530

    10-02-23 08:49PM - The First Day

--- src/main/java/com/techfuture/rmsb/RmsbApplication.java
+++ src/main/java/com/techfuture/rmsb/RmsbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RmsbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RmsbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RmsbApplication.class, args);
+    }
 
 }
